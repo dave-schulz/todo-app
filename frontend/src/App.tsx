81aa8346baf1eb669013142ccb41d7ac391ef22a
Davey Schulz <daveschulz@hotmail.nl>
2023-03-07 22:36:01 +0100

    Added the backend logic for Tasks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
 import React, { FC, ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 
 import { Dashboard } from './pages/dashboard/dashboard';
 import { customTheme } from './theme/customTheme';
 
+// client
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
